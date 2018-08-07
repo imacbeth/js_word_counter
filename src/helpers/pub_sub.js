8bed16934f57a35b4d67b2c91586245c379c70bc
Iona Macbeth <ionalmacbeth@gmail.com>
2018-08-07 09:56:31 +0100

    mvp complete

--- src/helpers/pub_sub.js
+++ src/helpers/pub_sub.js
@@ -1,3 +1,5 @@
+// PubSub is an object with key value pairs where functions are the values. similar to a class with only static methods in Java 
+
 const PubSub = {
   publish: function (channel, payload) {
       const event = new CustomEvent(channel, {
